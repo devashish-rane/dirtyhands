58342074d4eebac47ebc28903823c194506fcb62
devashishrane <ranedevashish2@gmail.com>
2026-02-03 20:04:55 +0530

    Initial commit

---
+++ src/main/java/com/devashish/dirtyhands/DirtyhandsApplication.java
@@ -0,0 +1,13 @@
+package com.devashish.dirtyhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirtyhandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirtyhandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devashish/dirtyhands/DirtyhandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devashish.dirtyhands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirtyhandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
